3388fc1b864bff9213bb6880dc79107038b5bfed
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2022-07-13 22:53:21 +0300

    add chart component code from turorial

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { Chart } from './components/chart/chart';
+
 export function App(): JSX.Element {
 	return (
 		<div>
-			Hello World
+			<Chart />
 		</div>
 	);
 }
