48a12542eac50bb1fdf3e5d63c41fb4c332f96c5
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2022-07-14 21:30:37 +0300

    implement weight-form

--- src/components/weight-form/weight-form.tsx
+++ src/components/weight-form/weight-form.tsx
@@ -1,22 +1,35 @@
 import React, { useState } from 'react';
 
+import { Button } from '../ui/button/button';
 import { Input } from '../ui/input/input';
+
+import * as styles from './weight-form.scss';
 
 export function WeightForm(): JSX.Element {
 	const [weight, setWeight] = useState<number | string>('');
 
 	return (
-		<form>
+		<form onSubmit={ handleFormSubmit } className={ styles.weightForm }>
 			<Input
+				className={ styles.weightInput }
 				type='number'
 				value={ weight }
-				placeholder='Weight'
+				placeholder={ 'Today\'s weight' }
 				onValueChange={ handleWeightInputChange }
+			/>
+
+			<Button
+				type='submit'
+				buttonText='Submit'
 			/>
 		</form>
 	);
 
 	function handleWeightInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
 		setWeight(event.target.value);
+	}
+
+	function handleFormSubmit(event: React.ChangeEvent<HTMLFormElement>): void {
+		event.preventDefault();
 	}
 }
