72467a6db8b6bdb4e4e05a57f9a16589e9df4baa
Washington Irving <iamwashingyou@gmail.com>
2023-05-14 22:48:56 +0300

    add port configuration for webpack dev server

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM node:18-alpine
 
 WORKDIR /usr/src/app
 
-ENV SERVER_URL=http://localhost:3000
-
 COPY package*.json ./
 
 RUN npm i
--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -1,9 +1,13 @@
+import { config } from 'dotenv';
+config();
+
 import { Configuration } from 'webpack';
 import { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
 const devConfig: Configuration = {
 	mode: 'development',
 	devServer: {
+		port: process.env['PORT'],
 		hot: true,
 		open: true,
 		historyApiFallback: true,
