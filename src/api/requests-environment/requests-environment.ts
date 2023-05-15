ca848b312150bbb8e272f059dba29c30333b5708
Washington Irving <iamwashingyou@gmail.com>
2023-05-15 13:41:55 +0300

    fix urls in re

--- src/api/requests-environment/requests-environment.ts
+++ src/api/requests-environment/requests-environment.ts
@@ -7,60 +7,52 @@ import {
 export class RequestsEnvironment implements IRequestsEnvironment {
 	private readonly _baseURL: string;
 
-	private readonly _authBaseURL: URL;
-	private readonly _userBaseURL: URL;
-	private readonly _weightBaseURL: URL;
-
 	public constructor(baseURL: string) {
 		this._baseURL = baseURL;
-
-		this._authBaseURL = new URL('auth', this._baseURL);
-		this._userBaseURL = new URL('user', this._baseURL);
-		this._weightBaseURL = new URL('weight', this._baseURL);
 	}
 
 	public signUp(): RequestMetainfo {
 		return this._makeRequestMetainfo(
-			new URL('sign-up', this._authBaseURL),
+			new URL('auth/sign-up', this._baseURL),
 			HTTPMethod.Post
 		);
 	}
 
 	public signIn(): RequestMetainfo {
 		return this._makeRequestMetainfo(
-			new URL('sign-in', this._authBaseURL),
+			new URL('auth/sign-in', this._baseURL),
 			HTTPMethod.Post
 		);
 	}
 
 	public refresh(): RequestMetainfo {
 		return this._makeRequestMetainfo(
-			new URL('refresh', this._authBaseURL),
+			new URL('auth/refresh', this._baseURL),
 			HTTPMethod.Post
 		);
 	}
 
 	public signOut(): RequestMetainfo {
 		return this._makeRequestMetainfo(
-			new URL('sign-out', this._authBaseURL),
+			new URL('auth/sign-out', this._baseURL),
 			HTTPMethod.Post
 		);
 	}
 
 	public allUsers(): RequestMetainfo {
-		return this._makeRequestMetainfo(new URL('all', this._userBaseURL));
+		return this._makeRequestMetainfo(new URL('user/all', this._baseURL));
 	}
 
 	public createWeight(): RequestMetainfo {
 		return this._makeRequestMetainfo(
-			this._weightBaseURL,
+			new URL('weight', this._baseURL),
 			HTTPMethod.Post
 		);
 	}
 
 	public updateWeight(): RequestMetainfo {
 		return this._makeRequestMetainfo(
-			this._weightBaseURL,
+			new URL('weight', this._baseURL),
 			HTTPMethod.Patch
 		);
 	}
